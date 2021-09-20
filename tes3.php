75dfa1130130b3c0a7cb1eb6e8036b64820feb9c
Faisyal Rachman <faipagun@gmail.com>
2021-09-20 15:49:39 +0800

    ubah tes3

--- tes3.php
+++ tes3.php
@@ -2,7 +2,7 @@
 include "koneksi.php";
 $sql = "SELECT id,bday,namalengkap FROM users WHERE MONTH(bday) = MONTH(CURDATE()) ORDER BY DATE_FORMAT(bday,'%m %d')";
 $query = mysql_query($sql);
-//$rows = mysql_fetch_array($query);
+//$rows = mysql_fetch_array($query); iayiaiyaiyi
 while($rows=mysql_fetch_array($query))
 {
 echo $rows['namalengkap'] . "  " . $rows['bday']."<br>";
